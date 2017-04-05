b9d991fde38dc0310d2c8143db6651e963be7a6d
Sharon <thewingthing@gmail.com>
2017-04-05 21:26:01 +1000

    update

--- 2015/1B/aCounter.py
+++ 2015/1B/aCounter.py
@@ -1,20 +1,20 @@
+import math
+
 x = {}
 
-def f(N, num, flipped):
-  if N == num:
-    return 1
-  elif num in x:
-    return x[num]
-  if num != int(str(num)[::-1]) and not flipped:
-    x[num] = min(f(N, num+1, False), f(N, int(str(num)[::-1]), True)) + 1
-  else:
-    x[num] = f(N, num+1, False) + 1
-  return x[num]
+def digits(n):
+  return int(math.log10(n)) + 1
+
+
 
 def main():
   for t in xrange(int(raw_input())):
     N = int(raw_input())
-    ans = f(N, 1, False)
+
+    if N <= 20:
+      ans = N
+
+    ans = f(N, 1, 1, False)
     print "Case #{}: {}".format(t+1, ans)
 
 if __name__ == "__main__":
